47dd22a0f7f0ad9c69aa3572bc3fb537d70fe595
Herman <hermanchen77@gmail.com>
2024-03-22 18:40:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Navbar'
+import TechStack from './TechStack'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <h3> Hi my name is Herman and I am a software Engineer. I love building apps and solving problems. Here are some of my works</h3>
+      <TechStack/>
+    </>
+  )
+}
+
+export default App
---
+++ src/TechStack.jsx
@@ -0,0 +1,14 @@
+function TechStack() {
+    return (
+        <div>
+            <h2>Here is my tech stack!</h2>
+            <div className="techStack">
+                <img className="techimg" src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'/>
+                <img className="techimg" src='https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'/>
+                <img className="techimg" src='https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'/>
+            </div>
+        </div>
+    )
+}
+
+export default TechStack
